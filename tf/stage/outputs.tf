adc9764b2762f66fd26a3946fa28154cc5bae7c2
Jinoo Sim <jinoosim@gmail.com>
2024-12-30 09:12:57 +0900

    Output 파일 설정

--- tf/stage/outputs.tf
+++ tf/stage/outputs.tf
@@ -1,24 +1,13 @@
-# # EC2(api server) - Private IP for bastion access
-# output "private_ip" {
-#     value = module.ec2.private_ip
-# }
+# VPC
 
-# # EC2(bastion) - EIP: // ec2-bastion 모듈에 output에 선언한 eip_ip
-# output "eip_ip" {
-#     value = module.ec2-bastion.eip_ip
-# }
+output "out_vpc_id" {
+    value = module.network.out_vpc_id
+}
 
-# # RDS - endppoint
-# output "rds_endpoint" {
-#     value = module.rds.rds_endpoint
-# }
+output "out_subnet_ids" {
+    value = module.network.out_subnet_ids
+}
 
-# # S3 - endpoint
-# output "s3_endpoint" {
-#     value = module.s3.s3_endpoint
-# }
-
-# # ALB - dnsname // aws 객체가 아니라 alb 모듈에서 가져온다
-# output "alb_dns_name" {
-#     value = module.alb.alb_dns_name
-# }
+output "out_eips" {
+    value = module.network.out_eips
+}
